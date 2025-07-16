095de9102be1977f257d279ae13c4d8847213381
Rolando Ramirez <7200ws@gmail.com>
2025-07-16 10:53:51 -0500

    fixes landing

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,6 @@
+export default function LandingPage() {
+    return (<div>
+        <h1>Bienvenido a la Página de Inicio</h1>
+        <p>Esta es la página de inicio de tu aplicación.</p>
+    </div>)
+}
