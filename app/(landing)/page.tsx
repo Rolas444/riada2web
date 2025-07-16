26e1ab8f854b447fcf0cc2cb97978600cded0a42
Rolando Ramirez <7200ws@gmail.com>
2025-07-16 11:18:56 -0500

    fixes remove

--- app/(landing)/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function LandingPage() {
-    return (<div>
-        <h1>Bienvenido a la Página de Inicio</h1>
-        <p>Esta es la página de inicio de tu aplicación.</p>
-    </div>)
-}
