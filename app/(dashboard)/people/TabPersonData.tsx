7cb29075f228da1994337ca48f26efd103c4b730
Rolando Ramirez <7200ws@gmail.com>
2025-08-06 16:25:40 -0500

    added space for address and phones

--- app/(dashboard)/people/TabPersonData.tsx
+++ app/(dashboard)/people/TabPersonData.tsx
@@ -17,22 +17,29 @@ export const TabPersonData = ({ person, onEditClick }: TabPersonDataProps) => {
     }
 
     return (
-        <div className="p-4 space-y-2 dark:text-gray-300">
-            <div className="flex justify-between items-center">
-                <h3 className="text-lg font-semibold">{`${person.name} ${person.middleName} ${person.lastName}`}</h3>
-                <button 
-                    onClick={() => onEditClick(person)}
-                    className="p-1.5 text-gray-500 rounded-md hover:bg-gray-100 hover:text-blue-600 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-blue-400 transition-colors"
-                    aria-label={`Editar a ${person.name}`}
-                >
-                    <Edit size={18} />
-                </button>
+        <div className="flex divide-x divide-gray-200 dark:divide-gray-700">
+            {/* Primera sección */}
+            <div className="w-1/2 p-4 space-y-2 dark:text-gray-300">
+                <div className="flex justify-between items-center">
+                    <h3 className="text-lg font-semibold">{`${person.name} ${person.middleName} ${person.lastName}`}</h3>
+                    <button 
+                        onClick={() => onEditClick(person)}
+                        className="p-1.5 text-gray-500 rounded-md hover:cursor-pointer hover:bg-gray-100 hover:text-blue-600 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-blue-400 transition-colors"
+                        aria-label={`Editar a ${person.name}`}
+                    >
+                        <Edit size={18} />
+                    </button>
+                </div>
+                <p><span className="font-semibold text-gray-600 dark:text-gray-400">Email:</span> {person.email || 'N/A'}</p>
+                <p><span className="font-semibold text-gray-600 dark:text-gray-400">Documento:</span> {person.typeDoc && person.docNumber ? `${person.typeDoc}: ${person.docNumber}` : 'N/A'}</p>
+                <p><span className="font-semibold text-gray-600 dark:text-gray-400">Nacimiento:</span> {person.birthday ? new Date(person.birthday).toLocaleDateString() : 'N/A'}</p>
+                <p><span className="font-semibold text-gray-600 dark:text-gray-400">Sexo:</span> {person.sex === 'M' ? 'Masculino' : 'Femenino'}</p>
+                <p><span className="font-semibold text-gray-600 dark:text-gray-400">Edad:</span> {CalculateAge(person.birthday)}</p>
             </div>
-            <p><span className="font-semibold text-gray-600 dark:text-gray-400">Email:</span> {person.email || 'N/A'}</p>
-            <p><span className="font-semibold text-gray-600 dark:text-gray-400">Documento:</span> {person.typeDoc && person.docNumber ? `${person.typeDoc}: ${person.docNumber}` : 'N/A'}</p>
-            <p><span className="font-semibold text-gray-600 dark:text-gray-400">Nacimiento:</span> {person.birthday ? new Date(person.birthday).toLocaleDateString() : 'N/A'}</p>
-            <p><span className="font-semibold text-gray-600 dark:text-gray-400">Sexo:</span> {person.sex === 'M' ? 'Masculino' : 'Femenino'}</p>
-            <p><span className="font-semibold text-gray-600 dark:text-gray-400">Edad:</span> {CalculateAge(person.birthday)}</p>
+            {/* Segunda sección */}
+            <div className="w-1/2 p-4">
+                {/* Aquí puedes agregar el contenido para la segunda sección */}
+            </div>
         </div>
     );
 }
